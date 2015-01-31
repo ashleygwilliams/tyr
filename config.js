b9bb315b1da684bde6064c0bcd7da3d574669780
Ashley Williams <ashley@bocoup.com>
2015-01-31 12:22:44 -0500

    add base files for tyr

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'http://0.0.0.0',
+  port: ':8080',
+  namespace: '/api/',
+
+  // list module folders here as strings in an order based on relations
+  // ex: ['authors', 'books', 'chapters']
+  resources: []
+};
